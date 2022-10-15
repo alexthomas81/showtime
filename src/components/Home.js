3ac9e0f5adb3c6956f9040611de907a85721c695
Alex Thomas <alexthomas81@gmail.com>
2022-10-15 11:40:44 +0530

    Favourites & Watch Later

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,9 @@
 import { Fragment, Suspense, useEffect, useState } from 'react';
-import moment from 'moment';
 import dynamic from 'next/dynamic'
 import '../styles/Home.css';
-import GradeIcon from '@material-ui/icons/Grade';
-import MovieIcon from '@material-ui/icons/Movie';
-import WatchLaterIcon from '@material-ui/icons/WatchLater';
 
 const Trailers = dynamic(() => import('./Trailers'))
+const Movies = dynamic(() => import('./Movies'))
 
 function Home() {
   const [width, setWidth] = useState(window.innerWidth);
@@ -23,6 +20,7 @@ function Home() {
   const handleWindowSizeChange = () => {
     setWidth(window.innerWidth);
   }
+  const [page, setPage] = useState('home')
 
   useEffect(() => {
     window.addEventListener('resize', handleWindowSizeChange);
@@ -68,32 +66,33 @@ function Home() {
       })
   }
 
-  const addFavourite = (id) => {
-    let newState = [...personal.favourites];
-    newState[newState.length] = id
-    let unique = [...new Set(newState)];
-    setPersonal({ ...personal, favourites: unique })
-  }
-
-  const watchLater = (id) => {
-    let newState = [...personal.watchLater];
-    newState[newState.length] = id
-    let unique = [...new Set(newState)];
-    setPersonal({ ...personal, watchLater: unique })
-  }
-
   const callHome = () => {
     setMovieLists(movies)
+    setPage('home')
   }
 
   const callFavourites = () => {
     let newList = movies.filter(element => { return personal.favourites.includes(element.id) })
     setMovieLists(newList)
+    setPage('favourites')
   }
 
   const callWatchLater = () => {
     let newList = movies.filter(element => { return personal.watchLater.includes(element.id) })
     setMovieLists(newList)
+    setPage('watchLater')
+  }
+
+  const updatePage = (page, type, array) => {
+    if (page == 'home') { setMovieLists(movies) }
+    if (page == 'favourites' && type == 'fav') {
+      let newList = movies.filter(element => { return array.includes(element.id) })
+      setMovieLists(newList)
+    }
+    if (page == 'watchLater' && type == 'wat') {
+      let newList = movies.filter(element => { return array.includes(element.id) })
+      setMovieLists(newList)
+    }
   }
 
   return (
@@ -105,29 +104,11 @@ function Home() {
         <input type="text" className="searchMovie" id="searchMovie" onChange={handleSearchMovie} />
       </div>
       <div className="Home-links">
-        <a onClick={() => callHome()}>Home</a>
-        <a onClick={() => callFavourites()}>Favourites</a>
-        <a onClick={() => callWatchLater()}>Watch Later</a>
+        <div onClick={() => callHome()}>Home</div>
+        <div onClick={() => callFavourites()}>Favourites</div>
+        <div onClick={() => callWatchLater()}>Watch Later</div>
       </div>
-      <div className="movieList">
-        {movieList.map((list, index) =>
-          <div className="movieItem" key={index}>
-            <div className="movieImage">
-              <img 
-                alt={list.title ? list.title : list.name}
-                src={width > 1024 ? `https://image.tmdb.org/t/p/w220_and_h330_face/${list.poster_path}` : `https://image.tmdb.org/t/p/w200/${list.poster_path}`}
-              />
-            </div>
-            <div className="movieName">{list.title ? list.title.substring(0, 26) : list.name.substring(0, 26)}</div>
-            <div className="movieRelease">{moment(list.release_date).format("LL")}</div>
-            <div className="movieLinks">
-              <GradeIcon onClick={() => addFavourite(list.id)} style={{ color: personal.favourites.includes(list.id) ? "red" : "black", cursor: "pointer" }} />
-              <WatchLaterIcon onClick={() => watchLater(list.id)} style={{ color: personal.watchLater.includes(list.id) ? "green" : "black", cursor: "pointer" }} />
-              <MovieIcon onClick={() => ShowTrailer(list.id)} style={{ color: "blue", cursor: "pointer" }} />
-            </div>
-          </div>
-        )}
-      </div>
+      <Movies movieList={movieList} page={page} updatePage={updatePage} width={width} personal={personal} setPersonal={setPersonal} ShowTrailer={ShowTrailer} />
 
       {trailerData ? (
         <Suspense>
